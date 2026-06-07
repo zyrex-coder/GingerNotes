65f1dafbcb23a51af2f28673e51560db44e7ab73
zyrex-coder <zyrexshaik@gmail.com>
2026-06-07 12:46:14 +0530

    V2

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'gingernotes-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './logo.jpg',
+  './manifest.json'
+];
+
+// Install a service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Update a service worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
